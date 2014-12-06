3865149c22d8cbe554c436665e6f022f71f3c48b
Todd <todd.mcbrearty@gmail.com>
2014-12-06 14:51:03 -0500

    inital commit

---
+++ src/Toddmcbrearty/Bladestrap/BladestrapServiceProvider.php
@@ -0,0 +1,71 @@
+<?php namespace Toddmcbrearty\Bladestrap;
+
+use Illuminate\Html\HtmlBuilder;
+use Illuminate\Support\ServiceProvider;
+use Toddmcbreaty\Bladestrap\BladestrapFormBuilder;
+
+class BladestrapServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('toddmcbrearty/bladestrap');
+	}
+
+	public function register() {
+		$this->registerHtmlBuilder();
+
+		$this->registerFormBuilder();
+	}
+
+
+	/**
+	 * Register the HTML builder instance.
+	 *
+	 * @return void
+	 */
+	protected function registerHtmlBuilder()
+	{
+		$this->app->bindShared('html', function($app)
+		{
+			return new HtmlBuilder($app['url']);
+		});
+	}
+
+	/**
+	 * Register the form builder instance.
+	 *
+	 * @return void
+	 */
+	protected function registerFormBuilder()
+	{
+		$this->app->bindShared('form', function($app)
+		{
+			$form = new BladestrapFormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+
+			return $form->setSessionStore($app['session.store']);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('html', 'form');
+	}
+
+}
